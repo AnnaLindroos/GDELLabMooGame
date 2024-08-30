27e5840dbbe89a7256ee0fb0ec0c6e092a0b2c2a
AnnaLindroos <92397151+AnnaLindroos@users.noreply.github.com>
2024-08-30 13:13:59 +0200

    Added Interface

---
+++ Interfaces/IPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabMooGameG.Interfaces;
+
+public interface IPlayer
+{
+    public string PlayerName { get; set; }
+    public int NumberOfGames { get; set; }
+    public int GuessesInTotal { get; set; }
+    void UpdatePlayerHighScore(int guesses);
+    double GetAverageGuesses();
+}
