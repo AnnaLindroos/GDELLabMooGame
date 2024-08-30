2a482cf7ef6a57432f5c2381316b6ffa7fc51c37
AnnaLindroos <92397151+AnnaLindroos@users.noreply.github.com>
2024-08-30 14:29:29 +0200

    Add files via upload
    added test files

---
+++ ConsoleIOTests.cs
@@ -0,0 +1,40 @@
+﻿using LabMooGameG.UI;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace MooTests;
+
+[TestClass]
+public class ConsoleIOTests
+{
+   [TestMethod]
+    public void ReadTest()
+    {
+        var input = "test input";
+        var consoleIO = new ConsoleIO();
+
+        using (var stringReader = new StringReader(input))
+        {
+            Console.SetIn(stringReader);
+
+            var result = consoleIO.Read();
+
+            Assert.AreEqual(input, result);
+        }
+    }
+
+    [TestMethod]
+    public void WriteLineTest()
+    {
+        var output = new StringWriter();
+        Console.SetOut(output);
+
+        var consoleIO = new ConsoleIO();
+        var message = "Hello, World!";
+
+        consoleIO.WriteLine(message);
+
+        Assert.AreEqual($"{message}{Environment.NewLine}", output.ToString());
+    }
+}
---
+++ DataContextTests.cs
@@ -0,0 +1,57 @@
+﻿using LabMooGameG.Data;
+using LabMooGameG.Models;
+
+namespace MooTests;
+
+[TestClass]
+public class DataContextTests
+{
+    private DataContext _dataContext;
+    private List<Player> _players;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        _dataContext = new DataContext(new DataContextConfig("testMooGame"));
+        _dataContext.CreateFile("Anna", 2);
+        _players = new List<Player>();
+    }
+
+    [TestMethod]
+    public void DataContextTest()
+    {
+        Assert.IsNotNull(_dataContext);
+    }
+
+    [TestMethod]
+    public void GetFilePathTest()
+    {
+        Assert.AreEqual("testMooGame", _dataContext.GetFilePath());
+    }
+
+    [TestMethod]
+    public void CheckIfPlayerExistsUpdateTest()
+    {
+        string playerName = "Anna";
+        int guesses = 2;
+
+        _dataContext.CheckIfPlayerExists(_players, "Anna", 2);
+        Assert.AreEqual(1, _players.Count);
+        Assert.AreEqual(playerName, _players[0].PlayerName);
+        Assert.AreEqual(guesses, _players[0].GuessesInTotal);
+    }
+
+    [TestMethod]
+    public void CheckIfPlayerExistsAddTest()
+    {
+        string playerName = "Anna";
+        int guesses = 5;
+        int newGuesses = 2;
+        Player existingPlayer = new Player(playerName, guesses);
+        _players.Add(existingPlayer);
+
+        _dataContext.CheckIfPlayerExists(_players, playerName, newGuesses);
+        Assert.AreEqual(1, _players.Count());
+        Assert.AreEqual(7, _players[0].GuessesInTotal);
+    }
+}
