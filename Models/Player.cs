d597a62e8a29845b74113f01c48fb396d0317521
AnnaLindroos <92397151+AnnaLindroos@users.noreply.github.com>
2024-08-30 13:37:58 +0200

    Refactored Player.cs and IPlayer

--- Models/Player.cs
+++ Models/Player.cs
@@ -1,15 +1,17 @@
-﻿namespace LabMooGameG.Models;
+﻿using LabMooGameG.Interfaces;
 
-public class Player 
+namespace LabMooGameG.Models;
+
+public class Player : IPlayer
 {
-    public string PlayerName { get; private set; }
-
-    public int NumberOfGames { get; private set; }
-
-    public int GuessesInTotal { get; private set; }
+    public string PlayerName { get; set; }
+    public int NumberOfGames { get; set; }
+    public int GuessesInTotal { get; set; }
 
     public Player(string playerName, int guesses)
     {
+        ArgumentNullException.ThrowIfNull(playerName);
+        ArgumentNullException.ThrowIfNull(guesses);
         PlayerName = playerName;
         NumberOfGames = 1;
         GuessesInTotal = guesses;
